285f484c16de4ccaeb9b45406476c8a6b2e839e5
dust demons <acanev2020@gmail.com>
2022-02-27 16:20:10 +0800

    update

--- ml-example.py
+++ ml-example.py
@@ -5,13 +5,12 @@ from  matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
 
-IMG_WIDTH=108
-IMG_HEIGHT=222
+IMG_WIDTH=888
+IMG_HEIGHT=888
 img_folder=r'picture'
 
 plt.figure(figsize=(20,20))
 test_folder=r'picture\test'
-
 
 # %%
 data_dir = 'picture'
@@ -94,3 +93,22 @@ print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
 )
+
+# %%
+import glob
+import os
+images = glob.glob('temp/*.png')
+specificClass = 'spec'
+
+for fileName in images:
+    img = tf.keras.utils.load_img(
+                        fileName, target_size=(img_height, img_width)
+                    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) 
+
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])   
+
+    if class_names[np.argmax(score)] == specificClass:          
+        os.rename(fileName, "done/"+os.path.basename(fileName))
